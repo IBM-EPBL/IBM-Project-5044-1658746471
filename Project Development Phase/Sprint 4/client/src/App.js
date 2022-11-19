b10a0f98eddc4de46c7b87bb4f640ba9b0d8c7ad
Ravi V <83852721+rgbRavi@users.noreply.github.com>
2022-11-19 23:39:19 +0530

    Sprint 4 completed

---
+++ Project Development Phase/Sprint 4/client/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import DashBoard from "./components/Dashboard";
+import AdminDashboard from "./components/AdminDashboard";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<DashBoard />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
